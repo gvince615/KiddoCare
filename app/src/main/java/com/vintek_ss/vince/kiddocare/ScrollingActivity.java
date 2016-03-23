082be3e95e1f20db27880b4390314d3923f64d87
Vincent, Gary <GaryVincent@quickenloans.com>
2016-03-23 17:55:12 -0400

    starting new view

--- app/src/main/java/com/vintek_ss/vince/kiddocare/ScrollingActivity.java
+++ app/src/main/java/com/vintek_ss/vince/kiddocare/ScrollingActivity.java
@@ -1,5 +1,13 @@
 package com.vintek_ss.vince.kiddocare;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -8,9 +16,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import static com.vintek_ss.vince.kiddocare.R.drawable.ic_boy;
+import static com.vintek_ss.vince.kiddocare.R.drawable.ic_girl;
+import static com.vintek_ss.vince.kiddocare.R.drawable.ic_launcher;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private static int ID = 0;
+
+    Bitmap bmp;
+    final static int cameraData = 0;
+    final String cItems[] = {"Generic Boy Image", "Generic Girl Image", "Take Picture"};
+
+    ImageView childImage;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +48,30 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle("Title");
+        collapsingToolbar.setContentScrimColor(getResources().getColor(R.color.black));
+
+        childImage = (ImageView) findViewById(R.id.iv_child_image);
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_take_child_picture);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton fab_take_pic = (FloatingActionButton) findViewById(R.id.fab_take_child_picture);
+        fab_take_pic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                takePic();
+            }
+        });
+
+        FloatingActionButton fab_add_card = (FloatingActionButton) findViewById(R.id.fab_add_card);
+        fab_add_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Card Added", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -38,13 +79,137 @@ public class ScrollingActivity extends AppCompatActivity {
         loadBackdrop();
     }
 
+    public void pickDate(View v) {
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        Dialog dateDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                monthOfYear++;
+                String newMonth = "";
+                switch (monthOfYear) {
+                    case 1:
+                        newMonth = "Jan";
+                        break;
+                    case 2:
+                        newMonth = "Feb";
+                        break;
+                    case 3:
+                        newMonth = "Mar";
+                        break;
+                    case 4:
+                        newMonth = "Apr";
+                        break;
+                    case 5:
+                        newMonth = "May";
+                        break;
+                    case 6:
+                        newMonth = "Jun";
+                        break;
+                    case 7:
+                        newMonth = "Jul";
+                        break;
+                    case 8:
+                        newMonth = "Aug";
+                        break;
+                    case 9:
+                        newMonth = "Sep";
+                        break;
+                    case 10:
+                        newMonth = "Oct";
+                        break;
+                    case 11:
+                        newMonth = "Nov";
+                        break;
+                    case 12:
+                        newMonth = "Dec";
+                        break;
+                }
+                if (ID == (R.id.et_Cbirthdate)) {
+                    TextView tv = (TextView) findViewById(R.id.et_Cbirthdate);
+                    tv.setText(year + "-" + newMonth + "-" + dayOfMonth);
+                }
+                if (ID == (R.id.et_child_Edate)) {
+                    TextView tv = (TextView) findViewById(R.id.et_child_Edate);
+                    tv.setText(year + "-" + newMonth + "-" + dayOfMonth);
+                }
+                if (ID == (R.id.et_DTAPdate)) {
+                    TextView tv = (TextView) findViewById(R.id.et_DTAPdate);
+                    tv.setText(year + "-" + newMonth + "-" + dayOfMonth);
+                }
+//                if (ID == (R.id.et_MMRdate)) {
+//                    TextView tv = (TextView) findViewById(R.id.et_MMRdate);
+//                    tv.setText(year + "-" + newMonth + "-" + dayOfMonth);
+//                }
+//                if (ID == (R.id.et_HEPBdate)) {
+//                    TextView tv = (TextView) findViewById(R.id.et_HEPBdate);
+//                    tv.setText(year + "-" + newMonth + "-" + dayOfMonth);
+//                }
+            }
+        }, mYear, mMonth, mDay);
+        dateDialog.show();
+        ID = v.getId();
+    }
+
     private void loadBackdrop() {
-        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        ImageView imageView = (ImageView) findViewById(R.id.iv_child_image);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_scrolling, menu);
         return true;
+    }
+    // Take a Pic
+    public void takePic() {
+        AlertDialog.Builder ab = new AlertDialog.Builder(ScrollingActivity.this);
+        ab.setTitle("Choose");
+        ab.setIcon(ic_launcher);
+
+        ab.setItems(cItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case (0):
+                        boyPic();
+                        break;
+                    case (1):
+                        girlPic();
+                        break;
+                    case (2):
+                        Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(i, cameraData);
+                        break;
+                }
+            }
+        });
+        ab.show();
+    }
+
+    public void boyPic() {
+        Drawable boy = getResources().getDrawable(ic_boy);
+        bmp = ((BitmapDrawable) boy).getBitmap();
+        childImage.setImageBitmap(bmp);
+    }
+
+    public void girlPic() {
+        Drawable girl = getResources().getDrawable(ic_girl);
+        bmp = ((BitmapDrawable) girl).getBitmap();
+        childImage.setImageBitmap(bmp);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // camera capture
+        if (resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bmp = (Bitmap) extras.get("data");
+            childImage.setImageBitmap(bmp);
+        }
     }
 }
