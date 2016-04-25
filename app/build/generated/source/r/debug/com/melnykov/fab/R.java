4be41ec647744c57b1546cf62de1a46833e7e9c3
Vincent, Gary <GaryVincent@quickenloans.com>
2016-04-25 07:14:47 -0400

    new main activity

--- app/build/generated/source/r/debug/com/melnykov/fab/R.java
+++ app/build/generated/source/r/debug/com/melnykov/fab/R.java
@@ -35,15 +35,15 @@ public final class R {
 	}
 	public static final class string {
 		public static final int define_FloatingActionButton = 0x7f07002d;
-		public static final int library_FloatingActionButton_author = 0x7f070055;
-		public static final int library_FloatingActionButton_authorWebsite = 0x7f070056;
-		public static final int library_FloatingActionButton_isOpenSource = 0x7f070057;
-		public static final int library_FloatingActionButton_libraryDescription = 0x7f070058;
-		public static final int library_FloatingActionButton_libraryName = 0x7f070059;
-		public static final int library_FloatingActionButton_libraryVersion = 0x7f07005a;
-		public static final int library_FloatingActionButton_libraryWebsite = 0x7f07005b;
-		public static final int library_FloatingActionButton_licenseId = 0x7f07005c;
-		public static final int library_FloatingActionButton_repositoryLink = 0x7f07005d;
+		public static final int library_FloatingActionButton_author = 0x7f070057;
+		public static final int library_FloatingActionButton_authorWebsite = 0x7f070058;
+		public static final int library_FloatingActionButton_isOpenSource = 0x7f070059;
+		public static final int library_FloatingActionButton_libraryDescription = 0x7f07005a;
+		public static final int library_FloatingActionButton_libraryName = 0x7f07005b;
+		public static final int library_FloatingActionButton_libraryVersion = 0x7f07005c;
+		public static final int library_FloatingActionButton_libraryWebsite = 0x7f07005d;
+		public static final int library_FloatingActionButton_licenseId = 0x7f07005e;
+		public static final int library_FloatingActionButton_repositoryLink = 0x7f07005f;
 	}
 	public static final class styleable {
 		public static final int[] FloatingActionButton = { 0x7f01001c, 0x7f0100c9, 0x7f0100ca, 0x7f0100cb, 0x7f0100cc, 0x7f0100cd, 0x7f0100ce, 0x7f0100cf, 0x7f0100d0, 0x7f0100d1, 0x7f0100d2, 0x7f0100d3, 0x7f01013b, 0x7f01013c };
